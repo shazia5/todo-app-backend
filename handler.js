624bcf62a9c8b740a1ac96f125d12110877b9736
Shazia <n_kirmani@yahoo.com>
2019-12-03 18:54:05 +0000

    error

--- handler.js
+++ handler.js
@@ -1,14 +1,15 @@
 const express=require("express");
 const serverlessHttp = require("serverless-http");
 const cors = require("cors");
-const bodyparser= require("body-parser");
+const bodyParser= require("body-Parser");
 const mysql = require("mysql");
+const uuid = require("uuid/v4");
 
 
 const app = express();
 app.use(cors());
 // allows Express to parse JSON ddata that is sent on the body of any requests
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -16,8 +17,7 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: "mytodolist"
 });
-
-
+// request not needed this for get / read 
 app.get("/tasks", function (request, response) {
   connection.query("SELECT * FROM task", function(err, data) {
     if (err) {
@@ -26,39 +26,48 @@ app.get("/tasks", function (request, response) {
         error: err
       });
     } else {
-      response.json({
+      response.status(200).send({
         tasks: data
       });
     }
   });
 });
-    
+
+app.delete("/tasks/:taskId", function (request, response) {
+  const id = request.params.id;
+  connection.query("DELETE FROM task WHERE id = ?", [id], function (err,data){ 
+    if (err) {
+      console.log("Error fetching task", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.sendstatus(200);
+    }
+  });
+});
+
+
 app.post("/tasks", function (request, response) {
   // Do the logic for saving the new task in the DB
   const task = request.body;
   // { text: "do the dishes", completed: true, date: "2019" }
-  response.status(201).send("Successfully created task: " + task.text);
+  response.status(201).send("Successfully created task: " + task.taskDescription);
 });
 
-// my fields from state in app.js
-// text
-// completed
-// dateDone
-// dateDue
-// id
-// possibly userid
 
-app.delete("/tasks/:taskId", function (request, response) {
-  const taskId = request.params.taskId;
-  response.status(200).send("Received request to Delete task with id " + taskId);
-});
 app.put("/tasks/:taskId", function (request, response) {
 //update task
   const taskId = request.params.taskId;
   const updatedTask = request.body;
   // eslint-disable-next-line no-undef
-  response.status(200).send(" Update task with id " + taskId + " with " + task.text + task.completed );
+  response.status(200).send(" Update task with id " + taskId + " with " + task.taskDescription + task.completed );
 });
 
 
 module.exports.tasks=serverlessHttp(app);
+
+
+
+
+
